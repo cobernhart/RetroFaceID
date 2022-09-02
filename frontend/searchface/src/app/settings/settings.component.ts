45329075270ce4525cdea46c3b67150f7104fcb3
Costin Bernhart <e11816878@student.tuwien.ac.at>
2022-09-02 17:34:33 +0200

    [ADDED] 1) Started implementing settings

--- frontend/searchface/src/app/settings/settings.component.ts
+++ frontend/searchface/src/app/settings/settings.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-  threshold = 1.6
+  threshold = 0
+  method = 'efCos'
+  optimalThresholds = [
+    {  name: 'efCos', limit : 1.61 },
+    { name: 'efCos+', limit: 1.62},
+    {name : 'efArc' , limit : 1.63},
+    {name : 'efArc+', limit : 1.64},
+    {name : 'arcFace', limit : 1.65}
+    ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateWeight(method: string){
+    console.log(method)
+    this.method = method
+    this.threshold = this.getOptimalThreshold(this.method)
+  }
+  getOptimalThreshold(method: string){
+    // @ts-ignore
+    return this.optimalThresholds.find(a => a.name == method).limit
+
   }
 
   rangeChange(t: any){
